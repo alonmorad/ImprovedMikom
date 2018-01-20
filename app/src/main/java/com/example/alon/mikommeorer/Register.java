b9b21c60167f11d448ec8b4f24289a49178bc8bd
alonmorad <34094812+alonmorad@users.noreply.github.com>
2018-01-20 19:19:12 +0200

    20.1 19:18 finished layout

--- app/src/main/java/com/example/alon/mikommeorer/Register.java
+++ app/src/main/java/com/example/alon/mikommeorer/Register.java
@@ -2,6 +2,8 @@ package com.example.alon.mikommeorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
