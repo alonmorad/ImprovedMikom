2e1ae6e70996fdd69c27bf0099f4384f7cd978c8
alonmorad <34094812+alonmorad@users.noreply.github.com>
2018-01-25 17:26:02 +0200

    25.1 17:25 start of map code

--- app/src/main/java/com/example/alon/mikommeorer/MapsActivity.java
+++ app/src/main/java/com/example/alon/mikommeorer/MapsActivity.java
@@ -1,8 +1,18 @@
 package com.example.alon.mikommeorer;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.telecom.Connection;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,6 +23,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private Location LastLocation;
+    private  static  final int MY_PERMISSION_REQUEST_CODE= 2980;
+    private  static  final int PLAY_SERVICES_RESOLUTION_REQUEST=300193;
+    private static int UPDATE_INTERVAL=5000;
+    private static int FASTEST_INTERVAL=3000;
+    private static int DISPLACEMENT=10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +40,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        setUpLocation();
     }
 
+    private void setUpLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED )
+        {
+            //request Runtime permission
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },MY_PERMISSION_REQUEST_CODE);
+        }
+        else
+        {
+            if (checkPlayServices())
+            {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+            }
+        }
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode= GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode!= ConnectionResult.SUCCESS)
+        {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+                GooglePlayServicesUtil.getErrorDialog(resultCode,this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            else {
+                Toast.makeText(this, "This Device Is not Supported", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return  false;
+        }
+        return true;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
