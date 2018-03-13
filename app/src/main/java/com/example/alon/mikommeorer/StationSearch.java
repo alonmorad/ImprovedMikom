42c80661c2692d1edfe2e7c915e0e855355df3e0
alonmorad <34094812+alonmorad@users.noreply.github.com>
2018-03-13 20:15:09 +0200

    13.3 bug fixed

--- app/src/main/java/com/example/alon/mikommeorer/StationSearch.java
+++ app/src/main/java/com/example/alon/mikommeorer/StationSearch.java
@@ -1,6 +1,8 @@
 package com.example.alon.mikommeorer;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,8 +15,15 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 import static java.security.AccessController.getContext;
 
@@ -27,6 +36,9 @@ public class StationSearch extends AppCompatActivity {
     private double station_location_lat;
     private double station_location_lng;
     private String stationChoosed;
+    AlertDialog alertDialog;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final String TAG = "Document:";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +68,7 @@ public class StationSearch extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 stationChoosed=String.valueOf(adapterView.getItemAtPosition(i));
                 Log.d("Stationname",  stationChoosed.toString());
-                Callback callback = new Callback<List<Station>>()
+                /*Callback callback = new Callback<List<Station>>()
                 {
                     @Override
                     public void onCallback(List<Station> stations) {
@@ -69,14 +81,30 @@ public class StationSearch extends AppCompatActivity {
                         }
                     }
                 };
-                stationPickServices.getStations(callback,stationChoosed);
-                Intent intent=new Intent(StationSearch.this,MapsActivity.class);
-                intent.putExtra("station_location_lat",station_location_lat);
-                intent.putExtra("station_location_lng",station_location_lng);
-                intent.putExtra("stationchoosed",stationChoosed);
-                intent.putExtra("data", linechoosed);
+                stationPickServices.getStations(callback,stationChoosed);*/
+                db.collection("stations").whereEqualTo("name",stationChoosed)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (DocumentSnapshot document : task.getResult()) {
+                                        station_location_lat=document.getGeoPoint("location").getLatitude();
+                                        station_location_lng=document.getGeoPoint("location").getLongitude();
+                                        Intent intent=new Intent(StationSearch.this,MapsActivity.class);
+                                        Log.d("locationstation", String.format("Your location was changed: %f / %f ", station_location_lat, station_location_lng));
+                                        intent.putExtra("station_location_lat",station_location_lat);
+                                        intent.putExtra("station_location_lng",station_location_lng);
+                                        intent.putExtra("stationchoosed",stationChoosed);
+                                        intent.putExtra("data", linechoosed);
+                                        startActivity(intent);
+                                    }
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
 
-                startActivity(intent);
             }
         });
         etSearch.addTextChangedListener(new TextWatcher() {
